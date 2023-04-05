70fd9844c18e798b75e0f6b19ab374a7d14931c3
JuliaZem98 <123088610+JuliaZem98@users.noreply.github.com>
2023-04-05 18:29:24 +0600

    Create 0002_category_product_category.py

---
+++ продукция/Миграции/0002_category_product_category.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('icon', models.ImageField(upload_to='')),
+                ('title', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='category',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='products.category'),
+        ),
+    ]
