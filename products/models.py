741c2c08e976eedefda6f36a14c4a53dbd3eb139
JuliaZem98 <123088610+JuliaZem98@users.noreply.github.com>
2023-04-05 18:25:57 +0600

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,29 +1,36 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Category(models.Model):
+    icon = models.ImageField()
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
 
 
 class Product(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    rate = models.FloatField(default=0.0)
-    created_date = models.DateField(auto_now_add=True)
-    modified_date = models.DateField(auto_now=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField()
+    price = models.FloatField()
+    rate = models.DecimalField(max_digits=10, decimal_places=1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True,
+                                 related_name="products")
 
     def __str__(self):
         return self.title
 
 
 class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
-    text = models.CharField(max_length=250)
-    content = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.product.title} - {self.text}'
-
-    class Meta:
-        ordering = ['-created_date']
-
+        return f'{self.author.username}_{self.text}'
